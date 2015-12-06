62838dbbf8d7140d04fc1ccecc4dd461cb57c541
Navarr Barnier <me@navarr.me>
2015-12-05 21:04:00 -0500

    Day 5

---
+++ day-05/part1.php
@@ -0,0 +1,68 @@
+<?php
+
+define('DEBUG', false);
+
+function is_nice($string)
+{
+    if (DEBUG) {
+        echo "Testing: '{$string}'", PHP_EOL;
+    }
+    $matchCountMap     = [
+        '/[aeiou]/i'   => 3,
+        '/([a-z])\1/i' => 1,
+    ];
+    $cantMatchCountMap = [
+        '/(ab|cd|pq|xy)/i'
+    ];
+
+    foreach ($matchCountMap as $pattern => $amount) {
+        if (DEBUG) {
+            echo "\tTest: {$pattern}: ";
+        }
+        $count = preg_match_all($pattern, $string);
+        if ($count < $amount) {
+            if (DEBUG) {
+                echo "Failed", PHP_EOL;
+            }
+            return false;
+        }
+        if (DEBUG) {
+            echo "Passed", PHP_EOL;
+        }
+    }
+
+    foreach ($cantMatchCountMap as $pattern) {
+        if (DEBUG) {
+            echo "\tTest NOT: {$pattern}: ";
+        }
+        $count = preg_match_all($pattern, $string);
+        if ($count > 0) {
+            if (DEBUG) {
+                echo "Failed", PHP_EOL;
+            }
+            return false;
+        }
+        if (DEBUG) {
+            echo "Passed", PHP_EOL;
+        }
+    }
+
+    return true;
+}
+
+// tests
+assert(is_nice('ugknbfddgicrmopn'));
+assert(is_nice('aaa'));
+assert(!is_nice('jchzalrnumimnmhp'));
+assert(!is_nice('haegwjzuvuyypxyu'));
+assert(!is_nice('dvszwmarrgswjxmb'));
+
+$contents = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$niceCount = 0;
+
+foreach ($contents as $potential) {
+    is_nice($potential) ? ++$niceCount : null;
+}
+
+echo "{$niceCount} nice strings",PHP_EOL;

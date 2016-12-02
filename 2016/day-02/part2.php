7f19058c7077ffe16159dd0e4b6af041dbd7ee8f
Navarr Barnier <Navarr.Barnier@briteskies.com>
2016-12-02 14:16:11 -0500

    2016 Day 2 Part 2

--- 2016/day-02/part2.php
+++ 2016/day-02/part2.php
@@ -3,26 +3,20 @@
 $digits = file('input.txt', FILE_IGNORE_NEW_LINES);
 
 $map = [
-    -1 => [-1 => '1', 0 => '2', 1 => '3'],
-    0  => [-1 => '4', 0 => '5', 1 => '6'],
-    1  => [-1 => '7', 0 => '8', 1 => '9'],
+    -2 => [2 => '1'],
+    -1 => [1 => '2', 2 => '3', 3 => '4'],
+    0  => [0 => '5', 1 => '6', 2 => '7', 3 => '8', 4 => '9'],
+    1  => [1 => 'A', 2 => 'B', 3 => 'C'],
+    2  => [2 => 'D'],
 ];
 
 $code = [];
 
-function require_between($min, $max, &$num)
-{
-    if ($num > $max) {
-        $num = $max;
-    }
-    if ($num < $min) {
-        $num = $min;
-    }
-}
-
 foreach ($digits as $digitInstruction) {
     $x = $y = 0;
     for ($i = 0, $l = strlen($digitInstruction); $i < $l; ++$i) {
+        $oldX = $x;
+        $oldY = $y;
         $instruction = $digitInstruction{$i};
         switch ($instruction) {
             case 'U':
@@ -38,10 +32,12 @@ foreach ($digits as $digitInstruction) {
                 ++$x;
                 break;
         }
-        require_between(-1, 1, $x);
-        require_between(-1, 1, $y);
+        if (!isset($map[$y]) || !isset($map[$y][$x])) {
+            $x = $oldX;
+            $y = $oldY;
+        }
     }
     $code[] = $map[$y][$x];
 }
 
-echo 'Code: ',implode('', $code),PHP_EOL;
+echo 'Code: ', implode('', $code), PHP_EOL;

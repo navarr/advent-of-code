02328a198c5f196d15ca2d2d774132a918504273
Navarr Barnier <me@navarr.me>
2021-12-05 13:46:17 -0500

    Add Assert

--- 2021/day-03/part2.php
+++ 2021/day-03/part2.php
@@ -21,52 +21,79 @@ function calculatePlaceBit(array $input, int $place): bool
     return $result[0] > $result[1] ? false : true;
 }
 
-foreach($input as $line) {
-    $length = strlen($line);
-    for($i = 0;$i < $length;++$i) {
-        $bit = $line[$i];
-        if (!isset($amounts[$i][$bit])) {
-            $amounts[$i][$bit] = 0;
+function calculate(array $input)
+{
+    foreach($input as $line) {
+        $length = strlen($line);
+        for($i = 0;$i < $length;++$i) {
+            $bit = $line[$i];
+            if (!isset($amounts[$i][$bit])) {
+                $amounts[$i][$bit] = 0;
+            }
+            $amounts[$i][$bit]++;
         }
-        $amounts[$i][$bit]++;
     }
+
+    $oxygenCandidates = $co2Candidates = $input;
+    $oxygenRating = $co2Rating = '';
+    $length = strlen($input[0]);
+
+    echo "String Length is {$length}",PHP_EOL;
+
+    for($place = 0;$place < $length;$place++) {
+        if (count($oxygenCandidates) === 1) {
+            $oxygenRating = bindec(reset($oxygenCandidates));
+            break;
+        }
+        $number = calculatePlaceBit($oxygenCandidates, $place) ? '1' : '0';
+        $oxygenCandidates = array_filter(
+            $oxygenCandidates,
+            static function ($candidate) use ($place, $number) {
+                return $candidate[$place] === $number;
+            }
+        );
+    }
+
+    for($place = 0;$place < $length;++$place) {
+        if (count($co2Candidates) === 1) {
+            $co2Rating = bindec(reset($co2Candidates));
+            break;
+        }
+        $number = calculatePlaceBit($co2Candidates, $place) ? '0' : '1';
+        $co2Candidates = array_filter(
+            $co2Candidates,
+            static function ($candidate) use ($place, $number) {
+                return $candidate[$place] === $number;
+            }
+        );
+    }
+
+    echo "Oxygen Rating: {$oxygenRating}",PHP_EOL;
+    echo "CO2 Rating: {$co2Rating}",PHP_EOL;
+
+    $answer = $oxygenRating * $co2Rating;
+    echo "Answer: ",$answer,PHP_EOL;
+
+    return $answer;
 }
 
-$oxygenCandidates = $co2Candidates = $input;
-$oxygenRating = $co2Rating = '';
-$length = strlen($input[0]);
+echo "---[ Test Data ]",PHP_EOL;
+$answer = calculate([
+    '00100',
+    '11110',
+    '10110',
+    '10111',
+    '10101',
+    '01111',
+    '00111',
+    '11100',
+    '10000',
+    '11001',
+    '00010',
+    '01010',
+]);
 
-echo "String Length is {$length}",PHP_EOL;
+assert($answer === 230);
 
-for($place = 0;$place < $length;$place++) {
-    if (count($oxygenCandidates) === 1) {
-        $oxygenRating = bindec(reset($oxygenCandidates));
-        break;
-    }
-    $number = calculatePlaceBit($oxygenCandidates, $place) ? '1' : '0';
-    $oxygenCandidates = array_filter(
-        $oxygenCandidates,
-        static function ($candidate) use ($place, $number) {
-            return $candidate[$place] === $number;
-        }
-    );
-}
-
-for($place = 0;$place < $length;++$place) {
-    if (count($co2Candidates) === 1) {
-        $co2Rating = bindec(reset($co2Candidates));
-        break;
-    }
-    $number = calculatePlaceBit($co2Candidates, $place) ? '0' : '1';
-    $co2Candidates = array_filter(
-        $co2Candidates,
-        static function ($candidate) use ($place, $number) {
-            return $candidate[$place] === $number;
-        }
-    );
-}
-
-echo "Oxygen Rating: {$oxygenRating}",PHP_EOL;
-echo "CO2 Rating: {$co2Rating}",PHP_EOL;
-
-echo "Answer: ",$oxygenRating*$co2Rating,PHP_EOL;
+echo "---[ Real Data ]",PHP_EOL;
+calculate($input);

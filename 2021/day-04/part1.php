2cb65f65db200f759b8eea85414e08c153013c28
Navarr Barnier <me@navarr.me>
2021-12-05 15:26:31 -0500

    Current marks building

--- 2021/day-04/part1.php
+++ 2021/day-04/part1.php
@@ -10,8 +10,10 @@ $marks = array_map(
     static function ($mark) {
         return (int)$mark;
     },
-    explode(',',next($input))
+    explode(',',current($input))
 );
+
+next($input); // Move pointer forward
 
 // Now, we're processing this file without empty lines - which means there are no spaces between boards
 // So we don't need to process those empty lines.  Each board is 5x5, so after 5, its a new board

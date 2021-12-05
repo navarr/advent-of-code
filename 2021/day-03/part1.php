c892cf2abeb30f58dad9e41a7dbcc10c32c7e33f
Navarr Barnier <me@navarr.me>
2021-12-05 12:50:37 -0500

    First Attempt

--- 2021/day-03/part1.php
+++ 2021/day-03/part1.php
@@ -2,4 +2,26 @@
 
 declare(strict_types=1);
 
-echo 'First Run Test';
+$input = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$amounts = [];
+
+foreach($input as $line) {
+    $length =strlen($line);
+    for($i = 0;$i < $length;++$i) {
+        $bit = $length{$i};
+        if (!isset($amounts[$i][$bit])) {
+            $amounts[$i][$bit] = 0;
+        }
+        $amounts[$i][$bit]++;
+    }
+}
+
+$gammaRate = '';
+
+foreach($amounts as $place => $data) {
+    $bit = $data[0] > $data[1] ? 0 : 1;
+    $gammaRate .= $bit;
+}
+
+echo bindec($gammaRate);
